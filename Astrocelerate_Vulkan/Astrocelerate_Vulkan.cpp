33da18d5b7bb8d5ebfba04bdb83ec3cf0a1e0a75
Duong Duy Nhat Minh <duongnhatminhbrvt@gmail.com>
2025-02-15 16:15:30 +0700

    Initialize Vulkan Rewrite branch

---
+++ Astrocelerate_Vulkan/Astrocelerate_Vulkan.cpp
@@ -0,0 +1,12 @@
+﻿// Astrocelerate_Vulkan.cpp : Defines the entry point for the application.
+//
+
+#include "Astrocelerate_Vulkan.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
