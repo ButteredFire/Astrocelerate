01243158b0bff651d75f20fa5f41c4c6854091c3
Duong Duy Nhat Minh <duongnhatminhbrvt@gmail.com>
2025-03-11 23:36:14 +0700

    Finalized shader stage and dynamic state initialization.

--- src/Vulkan/VkPipelineManager.cpp
+++ src/Vulkan/VkPipelineManager.cpp
@@ -9,22 +9,60 @@ GraphicsPipeline::GraphicsPipeline(VulkanContext& context):
 }
 
 GraphicsPipeline::~GraphicsPipeline() {
+	vkDestroyShaderModule(vkContext.logicalDevice, vertShaderModule, nullptr);
+	vkDestroyShaderModule(vkContext.logicalDevice, fragShaderModule, nullptr);
 }
 
 void GraphicsPipeline::init() {
-	loadShaders();
+	// Specify dynamic states
+	dynamicStates = {
+		VK_DYNAMIC_STATE_VIEWPORT,
+		VK_DYNAMIC_STATE_SCISSOR
+	};
+
+	// Shader stage
+	initShaderStage();
 }
 
 
 
-void GraphicsPipeline::loadShaders() {
+void GraphicsPipeline::initShaderStage() {
+	// Loads shader bytecode onto buffers
+		// Vertex shader
 	vertShaderBytecode = readFile("compiled_shaders/VertexShader.spv");
 	std::cout << "Loaded vertex shader! SPIR-V bytecode file size is " << vertShaderBytecode.size() << " (bytes).\n";
-	createShaderModule(vertShaderBytecode);
+	vertShaderModule = createShaderModule(vertShaderBytecode);
 
+		// Fragment shader
 	fragShaderBytecode = readFile("compiled_shaders/FragmentShader.spv");
 	std::cout << "Loaded fragment shader! SPIR-V bytecode file size is " << fragShaderBytecode.size() << " (bytes).\n";
-	createShaderModule(fragShaderBytecode);
+	fragShaderModule = createShaderModule(fragShaderBytecode);
+
+	// Creates shader stages
+		// Vertex shader
+	VkPipelineShaderStageCreateInfo vertStageInfo{};
+	vertStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	vertStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT; // Used to identify the create info's shader as the Vertex shader
+	vertStageInfo.module = vertShaderModule;
+	vertStageInfo.pName = "main"; // pName specifies the function to invoke, known as the entrypoint. This means that it is possible to combine multiple fragment shaders into a single shader module and use different entry points to differentiate between their behaviors. In this case we’ll stick to the standard `main`, however.
+
+	VkPipelineShaderStageCreateInfo fragStageInfo{};
+	fragStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+	fragStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+	fragStageInfo.module = fragShaderModule;
+	fragStageInfo.pName = "main";
+
+	shaderStages = {
+		vertStageInfo,
+		fragStageInfo
+	};
+}
+
+
+void GraphicsPipeline::initDynamicStates() {
+	dynamicStateCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+	dynamicStateCreateInfo.dynamicStateCount = static_cast<uint32_t>(dynamicStates.size());
+	dynamicStateCreateInfo.pDynamicStates = dynamicStates.data();
 }
 
 
--- src/Vulkan/VkPipelineManager.hpp
+++ src/Vulkan/VkPipelineManager.hpp
@@ -43,7 +43,8 @@ static inline std::vector<char> readFile(const std::string& fileName, const std:
 	// If file cannot open
 	if (!file.is_open()) {
 		throw std::runtime_error("Failed to open file " + enquote(fileName) + "!"
-			+ "\n" + "The file may not be in the correct working directory of " + enquote(workingDirectory)
+			+ "\n" + "The file may not be in the directory " + enquote(workingDirectory) + '.'
+			+ "\n" + "To change the working directory, please specify the full path to the file."
 		);
 	}
 
@@ -81,12 +82,26 @@ private:
 	VulkanContext& vkContext;
 
 	std::vector<char> vertShaderBytecode;
+	VkShaderModule vertShaderModule = VK_NULL_HANDLE;
+
 	std::vector<char> fragShaderBytecode;
+	VkShaderModule fragShaderModule = VK_NULL_HANDLE;
+	std::vector<VkPipelineShaderStageCreateInfo> shaderStages;
 
-	/* Loads compiled SPIR-V shader files. */
-	void loadShaders();
+	std::vector<VkDynamicState> dynamicStates;
+	VkPipelineDynamicStateCreateInfo dynamicStateCreateInfo{};
+
+	/* Creates the shader stage of the graphics pipeline from compiled SPIR-V shader files. */
+	void initShaderStage();
 
 
+	/* Initializes dynamic states.
+	* While most of the pipeline state needs to be baked into the pipeline state, a limited amount of the state can actually be changed without recreating the pipeline at draw time. Examples are the size of the viewport, line width and blend constants.
+	* Binding dynamic states via a Vk...CreateInfo structure causes the configuration of these values to be ignored and we will be able (and required) to specify the data at drawing time. This results in a more flexible setup.
+	*/
+	void initDynamicStates();
+	
+	
 	/* Creates a shader module to pass the code to the pipeline.
 	* @param bytecode: The SPIR-V shader file bytecode data.
 	* @return A VkShaderModule object.
